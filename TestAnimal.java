8a6a48454388ed5aa84e01792a59b4e426bd6187
OscarRoza <44257792+OscarRoza@users.noreply.github.com>
2018-10-18 10:52:44 +0200

    Update TestAnimal.java

--- TestAnimal.java
+++ TestAnimal.java
@@ -27,7 +27,7 @@ import java.util.*;
 
         //Informar de los datos animal1
           animal1.nace();
-          System.out.println("Me llamo " + animal1.getNombre() + " y tengo " + animal1.getEdad() + " anios");
+          System.out.println("mi nombre es " + animal1.getNombre() + " y tengo " + animal1.getEdad() + " anios");
 		}
 	}
   
